60cbf5cfc61c7e70da3792e50b3f1d034bd4f6a9
Tom Wilson <twilson650@gmail.com>
2023-01-17 07:21:12 -0800

    fixed syntax error

--- python/import-bulk.py
+++ python/import-bulk.py
@@ -13,7 +13,7 @@ headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
 print("Start program.")
 print(PATIENT_DATA_DIR)
 print("Starting loop.")
-for filename in os.listdir(PATIENT_DATA_DIR"):
+for filename in os.listdir(PATIENT_DATA_DIR):
     if filename.endswith(".json"):
         print(filename)
         with open(PATIENT_DATA_DIR + filename, "r", encoding="utf8") as bundle_file:
