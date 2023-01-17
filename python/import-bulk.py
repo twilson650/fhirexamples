8339b256b9aadd8be0b0d6d8f1073a585850e36a
Tom Wilson <twilson650@gmail.com>
2023-01-17 07:19:14 -0800

    patient data directory added to bulk import

--- python/import-bulk.py
+++ python/import-bulk.py
@@ -5,16 +5,17 @@ import requests
 #fhir server endpoint
 FHIR_URL = "http://localhost:8080/fhir/"
 
+PATIENT_DATA_DIR = os.getcwd() + "/fhirpatients"
+
 #fhir server json header content
 headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
 
 print("Start program.")
-print(os.getcwd() + "/src/codesamples/fhir-data/synthea_sample_data_fhir_r4_sep2019/fhir")
+print(PATIENT_DATA_DIR)
 print("Starting loop.")
-for filename in os.listdir(os.getcwd() + "/src/codesamples/fhir-data/synthea_sample_data_fhir_r4_sep2019/fhir"):
+for filename in os.listdir(PATIENT_DATA_DIR"):
     if filename.endswith(".json"):
-        dir = os.getcwd() + "/src/codesamples/fhir-data/synthea_sample_data_fhir_r4_sep2019/fhir/"
         print(filename)
-        with open(dir + filename, "r", encoding="utf8") as bundle_file:
+        with open(PATIENT_DATA_DIR + filename, "r", encoding="utf8") as bundle_file:
             data = bundle_file.read()
         r = requests.post(url = FHIR_URL, data = data.encode('utf-8'), headers = headers)
