a679f67c63130182efe1692375d9ae5d62f83a30
Tom Wilson <twilson@galileo.io>
2023-01-17 17:28:37 -0800

    test read and submit fhir bundle

--- python/import-bulk.py
+++ python/import-bulk.py
@@ -5,7 +5,7 @@ import requests
 #fhir server endpoint
 FHIR_URL = "http://localhost:8080/fhir/"
 
-PATIENT_DATA_DIR = os.getcwd() + "/data/fhirpatients"
+PATIENT_DATA_DIR = os.getcwd() + "/data/fhirpatients/"
 
 #fhir server json header content
 headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
@@ -13,9 +13,15 @@ headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
 print("Start program.")
 print(PATIENT_DATA_DIR)
 print("Starting loop.")
+count = 0
 for filename in os.listdir(PATIENT_DATA_DIR):
     if filename.endswith(".json"):
-        print(filename)
+        #print(filename)
         with open(PATIENT_DATA_DIR + filename, "r", encoding="utf8") as bundle_file:
             data = bundle_file.read()
         r = requests.post(url = FHIR_URL, data = data.encode('utf-8'), headers = headers)
+        print(r.status_code)
+        print(r.text)
+        count = count + 1
+        break
+print(str(count) + " patient records added")
